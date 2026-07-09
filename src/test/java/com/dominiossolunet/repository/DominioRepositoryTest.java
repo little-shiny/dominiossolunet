8ac9911f6d3a5a4d9a6200379cfedc024ad99820
Cristina ❤️ <mim0xina@gmail.com>
2026-07-09 20:58:45 +0200

    [TODO] Acabar de reformular el test con assertj

--- src/test/java/com/dominiossolunet/repository/DominioRepositoryTest.java
+++ src/test/java/com/dominiossolunet/repository/DominioRepositoryTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,12 +21,16 @@ public class DominioRepositoryTest {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     void deberia_encontrar_dominios_por_estado_y_fecha_antes_de() {
         // crear y guardar un Cliente
         Cliente cliente = new Cliente();
         cliente.setNombre("Pepito");
         cliente.setEmail("pepito99@pepito.es");
+        Cliente clienteGuardado = entityManager.persistAndFlush(cliente);
 
         // crear un Dominio asociado a ese cliente
         Dominio dominio = new Dominio();
@@ -32,8 +38,9 @@ public class DominioRepositoryTest {
         dominio.setEstado(Estado.ACTIVO);
         dominio.setFechaExpiracion(LocalDate.now().plusDays(10));
         dominio.setNombreDominio("pepitoysuscosas.com");
-        dominioRepository.save(dominio);
+        Dominio dominioGuardado = entityManager.persistAndFlush(dominio);
 
+        //TODO: cambiar a AssertionsJ
         //llamar al método derivado que se quiere probar
         List<Dominio> resultado = dominioRepository.findByEstadoAndFechaExpiracionBefore(
                 Estado.ACTIVO,
