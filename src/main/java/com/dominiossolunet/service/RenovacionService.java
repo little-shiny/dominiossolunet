cb26d70cf4d00707c9da67da83820ba1f9f3adfa
Cristina ❤️ <mim0xina@gmail.com>
2026-07-11 00:02:45 +0200

    Creación en curso de RenovacionService.java

---
+++ src/main/java/com/dominiossolunet/service/RenovacionService.java
@@ -0,0 +1,21 @@
+package com.dominiossolunet.service;
+
+import com.dominiossolunet.repository.DominioRepository;
+import lombok.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RenovacionService {
+    private final DominioRepository dominioRepository;
+
+    //Constructor
+    public RenovacionService(DominioRepository dominioRepository){
+        this.dominioRepository = dominioRepository;
+    }
+
+    public void enviarAvisosRenovacion(){
+        private
+    }
+
+}
