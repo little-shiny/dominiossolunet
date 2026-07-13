ea672daf52289c70325e1a0b9d1d97c3af1ccc57
Cristina ❤️ <mim0xina@gmail.com>
2026-07-13 22:08:28 +0200

    Se eliminan archivos duplicados del commit de recuperación

--- src/main/java/com/dominiossolunet/src/main/java/com/dominiossolunet/model/enums/Estado.java
+++
@@ -1,9 +0,0 @@
-package com.dominiossolunet.src.main.java.com.dominiossolunet.model.enums;
-
-public enum Estado {
-    ACTIVO,
-    AVISO_ENVIADO,
-    CLIENTE_ACEPTA,
-    EXPIRADO_SIN_RESPUESTA,
-    BAJA,
-}
--- src/main/java/com/dominiossolunet/src/main/java/com/dominiossolunet/model/enums/EstadoFacturacion.java
+++
@@ -1,6 +0,0 @@
-package com.dominiossolunet.src.main.java.com.dominiossolunet.model.enums;
-
-public enum EstadoFacturacion {
-    PENDIENTE_FACTURAR,
-    FACTURADO
-}
