e488d831c9dd2334eef65cbcdbb608216a1f69cb
Cristina ❤️ <mim0xina@gmail.com>
2026-07-13 21:32:12 +0200

    Completado el método procesarAvisos, solo falta el emailservice

--- src/main/java/com/dominiossolunet/service/RenovacionService.java
+++ src/main/java/com/dominiossolunet/service/RenovacionService.java
@@ -4,18 +4,15 @@ import com.dominiossolunet.model.Dominio;
 import com.dominiossolunet.model.TokenRenovacion;
 import com.dominiossolunet.model.enums.Estado;
 import com.dominiossolunet.repository.DominioRepository;
-import com.dominiossolunet.repository.TokenRenovacionRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * El servicio debe comprobar enb la bd los dominios con fecha de expiración en 30, 15, 5, 1
@@ -24,20 +21,17 @@ import java.util.UUID;
 @Service
 public class RenovacionService {
     private final DominioRepository dominioRepository; // Es final porque va en el constructor
-    private final TokenRenovacionRepository tokenRenovacionRepository;  //idem
+    private final TokenService tokenService;  //idem
 
     @Value("${renovacion.umbrales}")
     private List<Integer> umbrales;
 
-    @Value("${token.dias-expiracion}")
-    private int diasExpiracionToken;
-
     private int umbralMaximo; // Se asignará en el @Postconstruct
 
     //Constructor
-    public RenovacionService(DominioRepository dominioRepository, TokenRenovacionRepository tokenRenovacionRepository) {
+    public RenovacionService(DominioRepository dominioRepository, TokenService tokenService) {
         this.dominioRepository = dominioRepository;
-        this.tokenRenovacionRepository = tokenRenovacionRepository;
+        this.tokenService = tokenService;
     }
 
     // Ciclo de vida de Spring: tras el constructor se inyectan @Value y @Autowired. Después llama a los metodos
@@ -80,27 +74,11 @@ public class RenovacionService {
                 // actualizar el dominio con el nuevo estado y otro para el nuevo umbral aviso
                 dominio.setEstado(Estado.AVISO_ENVIADO);
                 dominio.setUltimoUmbralAvisado(umbralQueToca);
-                dominio.setUltimoAviso(LocalDate.now()); // debug
+                dominio.setUltimoAviso(LocalDate.now()); // dato informativo
 
-
+                TokenRenovacion token = tokenService.generarToken(dominio);
             }
         }
     }
-
-    //TODO: Genración de los tokens en su propio servicio, asignación temporal:
-    private TokenRenovacion generarToken(Dominio dominio) {
-
-        TokenRenovacion nuevoToken = new TokenRenovacion();
-        nuevoToken.setFechaCreacion(LocalDateTime.now());
-        nuevoToken.setFechaExpiracion(LocalDateTime.now().plusDays(diasExpiracionToken));
-        nuevoToken.setDominio(dominio);
-        nuevoToken.setToken(UUID.randomUUID().toString());
-        nuevoToken.setUsado(false);
-
-        return tokenRenovacionRepository.save(nuevoToken);
-    }
-
     //TODO:EmailService
-
-
 }
