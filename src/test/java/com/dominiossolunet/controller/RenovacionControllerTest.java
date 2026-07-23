9b317b96308face62e4390e5a5323f197d9b6fc3
Cristina ❤️ <mim0xina@gmail.com>
2026-07-23 22:59:52 +0200

    Creación de clases de Test. TODO completar los test y corregir error en TokenRenovacionRepository por la tabla nueva, así como crear dicho repository

---
+++ src/test/java/com/dominiossolunet/controller/RenovacionControllerTest.java
@@ -0,0 +1,7 @@
+package com.dominiossolunet.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RenovacionControllerTest {
+
+}
---
+++ src/test/java/com/dominiossolunet/service/TokenServiceTest.java
@@ -0,0 +1,54 @@
+package com.dominiossolunet.service;
+
+import com.dominiossolunet.dto.ResultadoValidacionRec;
+import com.dominiossolunet.model.Cliente;
+import com.dominiossolunet.model.TokenCliente;
+import com.dominiossolunet.model.enums.ResultadoValidacion;
+import com.dominiossolunet.repository.TokenRenovacionRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TokenServiceTest {
+
+    @Mock
+    private TokenRenovacionRepository tokenRenovacionRepository;
+
+    @InjectMocks
+    private TokenService tokenService;
+
+    // TESTS
+    @Test
+    void validarToken_cuandoTokenValido_devuelveResultadoValido(){
+
+        Cliente cliente = new Cliente();
+        cliente.setNombre("Ana");
+        cliente.setEmail("ana@ana.com");
+
+        //Token valido
+        TokenCliente token = new TokenCliente();
+        token.setToken("abc123");
+        token.setUsado(false);
+        token.setFechaCreacion(LocalDateTime.now());
+        token.setFechaExpiracion(LocalDateTime.now().plusDays(1));
+        token.setCliente(cliente);
+
+        when(tokenRenovacionRepository.findByToken("abc123")).thenReturn(Optional.of(token));
+
+        ResultadoValidacionRec resultado = tokenService.validarToken("abc123");
+
+        assertThat(resultado.resultado()).isEqualTo(ResultadoValidacion.VALIDO);
+        assertThat(resultado.token()).isEqualTo(token);
+        assertThat(resultado.token().getCliente().getNombre()).isEqualTo("Ana");
+
+    }
+}
