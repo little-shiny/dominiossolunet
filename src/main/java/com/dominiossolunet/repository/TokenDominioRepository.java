9f6f4af5d24f28ddd34ceb7895a3d0c8472abb0f
Cristina ❤️ <mim0xina@gmail.com>
2026-07-25 23:32:05 +0200

    Actualización con método patra buscar por lista de id_dominio

--- src/main/java/com/dominiossolunet/repository/TokenDominioRepository.java
+++ src/main/java/com/dominiossolunet/repository/TokenDominioRepository.java
@@ -5,10 +5,19 @@ import com.dominiossolunet.model.TokenDominio;
 import com.dominiossolunet.model.enums.EstadoAvisoRenovacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenDominioRepository extends JpaRepository<TokenDominio, Integer> {
     Optional<TokenDominio> findByDominio(Dominio dominio);
 
     Optional<TokenDominio> findByEstadoAvisoRenovacion(EstadoAvisoRenovacion estadoAvisoRenovacion);
+
+    /**
+     * Query que proporciona una lista de TokenDominio a partir de una lista de id_dominio
+     * @param ids
+     * @return lista TokenDominio
+     */
+    List<TokenDominio> findByDominio_IdIn(List<Integer> ids);
+
 }
